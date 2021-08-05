eff41d409dba839cc5892bc337173cc9fb216220
soyesdev <mungmoong16@naver.com>
2021-08-05 23:56:12 +0900

    feat: stone game 문제 풀이 추가

---
+++ LeetCode/src/challenge/august/stone_game/Solution.java
@@ -0,0 +1,16 @@
+package challenge.august.stone_game;
+
+/*
+ * About
+ *
+ * Author: soyesdev@GitHub
+ * Date  : 2021-08-05
+ * URL   : https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/613/week-1-august-1st-august-7th/3870/
+ *
+ */
+
+public class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
